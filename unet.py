444b6f40ba86ae28fb9e32cb275b53399c57884e
mtestouri <mehditestouri96@gmail.com>
2020-04-01 17:25:08 +0200

    dropout & norm & dataset

--- unet.py
+++ unet.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 import math
 import numpy as np
 
@@ -15,16 +16,16 @@ class UnetSegmenter(Segmenter):
         self.model = Unet().to(self.device)
 
     def train(self, x_train, y_train, model_file=None):
-        # dataset
+        # create train set
         batch_size = 1
         train_set = ImgSet(x_train, y_train)
         data_loader = DataLoader(dataset=train_set, batch_size=batch_size,
                                  num_workers=2)
         # training parameters
         num_epochs = 2
-        learning_rate = 0.001
+        learning_rate = 0.0001
         criterion = nn.BCEWithLogitsLoss()
-        optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
         # train loop
         n_iterations = math.ceil(len(train_set)/batch_size)
         for epoch in range(num_epochs):
@@ -52,69 +53,87 @@ class UnetSegmenter(Segmenter):
         # load model
         if model_file:
             self.model.load_state_dict(torch.load(model_file))
-        self.model = self.model.to('cpu') #TODO fix mem problem with CUDA
-        # predictions
+        # create test set
+        test_set = ImgSet(x_test)
+        data_loader = DataLoader(dataset=test_set, batch_size=1, num_workers=2)
+        # init predicted masks
         im_h = x_test.shape[1]
         im_w = x_test.shape[2]
         y_preds = np.empty((len(x_test), im_h, im_w, 2), dtype=np.float32)
-        for i, x in enumerate(x_test):
+        # compute predictions
+        for i, x in enumerate(data_loader):
             with torch.no_grad():
-                x = torch.from_numpy(x.reshape(1, 3, im_h, im_w))
-                y_pred = self.model(x)
-                y_pred = F.softmax(y_pred, dim=1) #TODO sum not 1 !!
-                y_pred = y_pred.squeeze(0).reshape(im_h, im_w, 2)
-                y_preds[i] = y_pred.cpu().numpy()
-            print(f'segmentation: {i+1}/{len(x_test)}', end='\r')
+                x = x.to(self.device)
+                y_pred = F.softmax(self.model(x), dim=1) #TODO sum prob of classes is not 1
+                y_preds[i] = y_pred.squeeze(0).reshape(im_h, im_w, 2).cpu().numpy()
+            print(f'segmentation: {i+1}/{len(test_set)}', end='\r')
         print('')
-        self.model = self.model.to('cuda')
         return y_preds
 
 class Unet(nn.Module):
-    def __init__(self):
+    def __init__(self, init_depth=32, n_classes=2):
         super(Unet, self).__init__()
         # encoder
-        self.conv1 = Conv3x3(3, 32)
-        self.conv2 = Conv3x3(32, 64)
-        self.conv3 = Conv3x3(64, 128)
-        self.conv4 = Conv3x3(128, 256)
-        self.conv5 = Conv3x3(256, 512, use_pool=False)
+        self.pool = nn.MaxPool2d((2, 2))
+        in_ch = 3
+        out_ch = init_depth
+        self.conv1 = Conv3x3(in_ch, out_ch)
+        in_ch = out_ch
+        out_ch = out_ch*2
+        self.conv2 = Conv3x3(in_ch, out_ch)
+        in_ch = out_ch
+        out_ch = out_ch*2
+        self.conv3 = Conv3x3(in_ch, out_ch)
+        in_ch = out_ch
+        out_ch = out_ch*2
+        self.conv4 = Conv3x3(in_ch, out_ch)
+        in_ch = out_ch
+        out_ch = out_ch*2
+        self.conv5 = Conv3x3(in_ch, out_ch)
         # decoder
-        self.up_conv6 = UpConv2x2(512, 256)
-        self.up_conv7 = UpConv2x2(256, 128)
-        self.up_conv8 = UpConv2x2(128, 64)
-        self.up_conv9 = UpConv2x2(64, 32)
-        self.conv10 = Conv1x1(32, 2)
+        in_ch = out_ch
+        out_ch = int(out_ch/2)
+        self.up_conv6 = UpConv2x2(in_ch, out_ch)
+        in_ch = out_ch
+        out_ch = int(out_ch/2)
+        self.up_conv7 = UpConv2x2(in_ch, out_ch)
+        in_ch = out_ch
+        out_ch = int(out_ch/2)
+        self.up_conv8 = UpConv2x2(in_ch, out_ch)
+        in_ch = out_ch
+        out_ch = int(out_ch/2)
+        self.up_conv9 = UpConv2x2(in_ch, out_ch)
+        in_ch = out_ch
+        self.conv10 = Conv1x1(in_ch, n_classes)
     
     def forward(self, x):
         # encoder
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
+        x1 = self.conv1(x)
+        x = self.pool(x1)
+        x2 = self.conv2(x)
+        x = self.pool(x2)
+        x3 = self.conv3(x)
+        x = self.pool(x3)
+        x4 = self.conv4(x)
+        x = self.pool(x4)
         x = self.conv5(x)
         # decoder
-        x = self.up_conv6(x, self.conv4.skip_x)
-        x = self.up_conv7(x, self.conv3.skip_x)
-        x = self.up_conv8(x, self.conv2.skip_x)
-        x = self.up_conv9(x, self.conv1.skip_x)
+        x = self.up_conv6(x, x4)
+        x = self.up_conv7(x, x3)
+        x = self.up_conv8(x, x2)
+        x = self.up_conv9(x, x1)
         x = self.conv10(x)
         return x
 
 class Conv3x3(nn.Module):
-    def __init__(self, in_ch, out_ch, use_pool=True):
+    def __init__(self, in_ch, out_ch):
         super(Conv3x3, self).__init__()
         self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
         self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
-        self.skip_x = torch.Tensor
-        self.use_pool = use_pool
-        if self.use_pool:
-            self.pool = nn.MaxPool2d((2, 2))
+        #self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
-        self.skip_x = F.relu(self.conv2(F.relu(self.conv1(x))))
-        if self.use_pool:
-            return self.pool(self.skip_x)
-        return self.skip_x
+        return F.relu(self.conv2(F.relu(self.conv1(x))))
 
 class Conv1x1(nn.Module):
     def __init__(self, in_ch, out_ch):
@@ -133,19 +152,29 @@ class UpConv2x2(nn.Module):
 
     def forward(self, x, skip_x):
         x = self.up(x)
-        x = torch.cat([x, skip_x], 1)
+        x = torch.cat([x, skip_x], dim=1)
         return F.relu(self.conv2(F.relu(self.conv1(x))))
 
 class ImgSet(Dataset):
-    def __init__(self, x, y):
-        x = x.reshape(x.shape[0], 3, x.shape[1], x.shape[2])
-        y = y.reshape(y.shape[0], 2, y.shape[1], y.shape[2])
-        self.x = torch.from_numpy(x)
-        self.y = torch.from_numpy(y)
+    def __init__(self, x, y=None):
         self.n_samples = x.shape[0]
-
+        # images
+        x = x.reshape(x.shape[0], 3, x.shape[1], x.shape[2])
+        self.x = torch.from_numpy(x)#/255
+        #tf = transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.25, 0.25, 0.25))
+        #for i in range(len(self.x)):
+        #    self.x[i] = tf(self.x[i])
+        # masks
+        if y is not None:
+            y = y.reshape(y.shape[0], 2, y.shape[1], y.shape[2])
+            self.y = torch.from_numpy(y)
+        else:
+            self.y = None
+        
     def __getitem__(self, index):
-        return self.x[index], self.y[index]
+        if self.y is not None:
+            return self.x[index], self.y[index]
+        return self.x[index]
 
     def __len__(self):
         return self.n_samples
